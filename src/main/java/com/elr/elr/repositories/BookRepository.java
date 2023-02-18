04c9d7467c4664b4eb5bf875944011bb849b1d0f
Elimasrojas <elimasrojas@gmail.com>
2023-02-17 20:49:03 -0500

    Introduccion  a SpringDataJpa

---
+++ src/main/java/com/elr/elr/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package com.elr.elr.repositories;
+
+import com.elr.elr.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
