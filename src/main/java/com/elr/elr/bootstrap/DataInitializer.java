1069690b4cef25710d7e690fbf15635d09c56df1
Elimasrojas <elimasrojas@gmail.com>
2023-02-17 22:15:57 -0500

    section 5 @Profile y conf del properties

--- src/main/java/com/elr/elr/bootstrap/DataInitializer.java
+++ src/main/java/com/elr/elr/bootstrap/DataInitializer.java
@@ -3,8 +3,10 @@ package com.elr.elr.bootstrap;
 import com.elr.elr.domain.Book;
 import com.elr.elr.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile({"local", "default"}) // corre en el perfil local o default (si el perfil no ha sido especificado)
 @Component
 public class DataInitializer implements CommandLineRunner {
 
@@ -16,13 +18,10 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        bookRepository.deleteAll();
+
         Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse");
-
-        System.out.println("_Id: " + bookDDD.getId() );
-
         Book savedDDD = bookRepository.save(bookDDD);
-
-        System.out.println("Id: " + savedDDD.getId() );
 
         Book bookSIA = new Book("Spring In Action", "234234", "Oriely");
         Book savedSIA = bookRepository.save(bookSIA);
